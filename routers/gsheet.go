9e10446ec03ceb9754bd83bb7ef26df9d29a1f43
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-24 00:31:53 +0300

    Add GsheetRoutes for retrieving Google Sheet title

---
+++ routers/gsheet.go
@@ -0,0 +1,10 @@
+package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/ibtikar-org-tr/iforgot/handlers"
+)
+
+func GsheetRoutes(router *gin.Engine) {
+	router.GET("/gsheet", handlers.GetSheetTitle)
+}
