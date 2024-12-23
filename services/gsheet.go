373340060db6f3d01649d748b5f62fcaaf5e8dfc
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-24 00:34:54 +0300

    Refactor Google Sheets service initialization to use global service variable from initializers

--- services/gsheet.go
+++ services/gsheet.go
@@ -1,61 +1,20 @@
 package services
 
 import (
-	"context"
 	"fmt"
-	"log"
-	"os"
-	"path/filepath"
 
 	"github.com/ibtikar-org-tr/iforgot/initializers"
-	"google.golang.org/api/option"
 	"google.golang.org/api/sheets/v4"
 )
 
-var srv *sheets.Service
-
-func init() {
-	// Load Env
-	initializers.LoadEnv()
-
-	// Path to your service account key file
-	serviceAccountFile := os.Getenv("GOOGLE_API_JSON_PATH")
-	if serviceAccountFile == "" {
-		log.Fatalf("GOOGLE_API_JSON_PATH environment variable is not set")
-	}
-
-	// Normalize the file path
-	serviceAccountFile = filepath.Clean(serviceAccountFile)
-
-	// Read the service account JSON file
-	data, err := os.ReadFile(serviceAccountFile)
-	if err != nil {
-		log.Fatalf("Unable to read client secret file: %v\n", err)
-	}
-
-	// Define required scopes
-	scopes := []string{
-		"https://www.googleapis.com/auth/spreadsheets",
-		"https://www.googleapis.com/auth/drive",
-	}
-
-	// Initialize the Sheets service
-	srv, err = sheets.NewService(context.Background(), option.WithCredentialsJSON(data), option.WithScopes(scopes...))
-	if err != nil {
-		log.Fatalf("Unable to create Sheets client: %v\n", err)
-	}
-
-	fmt.Println("Sheets service initialized successfully")
-}
-
 func GetGSheet(sheetID string) *sheets.Spreadsheet {
-	if srv == nil {
+	if initializers.Srv == nil {
 		fmt.Println("Sheets service is not initialized")
 		return nil
 	}
 
 	// Open the spreadsheet using its ID
-	spreadsheet, err := srv.Spreadsheets.Get(sheetID).Do()
+	spreadsheet, err := initializers.Srv.Spreadsheets.Get(sheetID).Do()
 	if err != nil {
 		fmt.Printf("Unable to retrieve data from sheet: %v\n", err)
 		return nil
@@ -80,7 +39,7 @@ func GetContactsPage(sheetID string) [][]interface{} {
 
 	for _, sheet := range sheet.Sheets {
 		if sheet.Properties.Title == "Contacts" {
-			resp, err := srv.Spreadsheets.Values.Get(sheetID, sheet.Properties.Title).Do()
+			resp, err := initializers.Srv.Spreadsheets.Values.Get(sheetID, sheet.Properties.Title).Do()
 			if err != nil {
 				fmt.Printf("Unable to retrieve data from sheet: %v\n", err)
 				return nil
