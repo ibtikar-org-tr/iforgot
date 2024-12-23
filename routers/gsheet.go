302fc0077baaad411746709d2c9b35a470cb5dbb
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-24 01:35:10 +0300

    Replace first line route with search value route in GsheetRoutes

--- routers/gsheet.go
+++ routers/gsheet.go
@@ -7,5 +7,5 @@ import (
 
 func GsheetRoutes(router *gin.Engine) {
 	router.GET("/gsheet", handlers.GetSheetTitle)
-	router.GET("/firstline", handlers.GetSheetFirstPageFirstLine)
+	router.GET("/search", handlers.SearchValueInSheet)
 }
