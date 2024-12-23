0537e1c42deb3f4f95a6b26c4f0f2036059d1f12
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-24 02:26:08 +0300

    Add CheckLastSent function to manage session frequency and update logic

--- services/ip.go
+++ services/ip.go
@@ -1,6 +1,8 @@
 package services
 
 import (
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/ibtikar-org-tr/iforgot/models"
 	"github.com/ibtikar-org-tr/iforgot/repositories"
@@ -13,4 +15,30 @@ func GetSessionByIP(c *gin.Context) (*models.Session, error) {
 		return nil, err
 	}
 	return session, nil
+}
+
+func CheckLastSent(session *models.Session) bool {
+	if session.Frequency == 0 {
+		return true
+	}
+	if time.Since(session.LastSent) > 24*time.Hour {
+		session.Frequency = 0
+		repositories.UpdateSession(session)
+		return true
+	}
+
+	waitTimes := []int{1, 5, 15, 30, 60} // in minutes
+	waitTime := waitTimes[session.Frequency-1]
+	if session.Frequency > len(waitTimes) {
+		waitTime = waitTimes[len(waitTimes)-1]
+	}
+
+	lastSent := session.LastSent
+
+	if lastSent.Add(time.Duration(waitTime) * time.Minute).Before(time.Now()) {
+		repositories.UpdateSession(session)
+		return true
+	}
+
+	return false
 }
