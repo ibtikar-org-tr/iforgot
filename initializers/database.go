bfa64b6ad316ce29778835db34aa5842c1341f56
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-24 01:53:06 +0300

    Add database connection initializer using GORM for PostgreSQL

---
+++ initializers/database.go
@@ -0,0 +1,25 @@
+package initializers
+
+import (
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	var err error
+	// Connect to database
+	dsn := "host=" + os.Getenv("DB_HOST") +
+		" user=" + os.Getenv("DB_USER") +
+		" password=" + os.Getenv("DB_PASSWORD") +
+		" dbname=" + os.Getenv("DB_NAME") +
+		" port=" + os.Getenv("DB_PORT") +
+		" sslmode=disable TimeZone=Europe/Istanbul"
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+}
