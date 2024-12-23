ea83856600c3f03b581ad7a209f2f1e7a3ad7284
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-24 02:09:18 +0300

    Rename handler to service and update GetSessionByIP function to return error

--- services/ip.go
+++ services/ip.go
@@ -1,19 +1,16 @@
-package handlers
+package services
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 	"github.com/ibtikar-org-tr/iforgot/models"
 	"github.com/ibtikar-org-tr/iforgot/repositories"
 )
 
-func GetIP(c *gin.Context) *models.Session {
+func GetSessionByIP(c *gin.Context) (*models.Session, error) {
 	ip := c.ClientIP()
 	session, err := repositories.GetSessionByIP(ip)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return nil
+		return nil, err
 	}
-	return session
+	return session, nil
 }
