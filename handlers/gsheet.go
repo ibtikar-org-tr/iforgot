62e6f295f1093f30a76f98b4c907f1cb5475a79e
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-24 01:34:55 +0300

    Add error handling to GetSheetTitle and implement SearchValueInSheet function

--- handlers/gsheet.go
+++ handlers/gsheet.go
@@ -9,6 +9,25 @@ import (
 
 func GetSheetTitle(c *gin.Context) {
 	sheetID := c.Query("sheetID")
-	sheetName := services.GetSheetTitle(sheetID)
+	sheetName, err := services.GetSheetTitle(sheetID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
 	c.JSON(http.StatusOK, gin.H{"sheetName": sheetName})
+}
+
+func SearchValueInSheet(c *gin.Context) {
+	sheetID := c.Query("sheetID")
+	valueToSearch := c.Query("value")
+	pageName := c.Query("page")
+	lastColumn := c.Query("last")
+
+	searchResult, err := services.SearchValueInSheet(valueToSearch, sheetID, pageName, lastColumn)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"searchResult": searchResult})
 }
