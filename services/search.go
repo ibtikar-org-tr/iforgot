7f588ef043ad79aec27b5331c882226d603e1172
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-25 13:12:19 +0300

    Add type validation for SearchMain function to ensure valid input values

--- services/search.go
+++ services/search.go
@@ -8,6 +8,25 @@ import (
 	"github.com/ibtikar-org-tr/iforgot/initializers"
 )
 
+// Define a custom type for the allowed values
+type ValueType string
+
+const (
+	Number ValueType = "number"
+	Mail   ValueType = "mail"
+	Phone  ValueType = "phone"
+)
+
+// Validate the typeOfValue
+func validateTypeOfValue(typeOfValue string) (ValueType, error) {
+	switch ValueType(typeOfValue) {
+	case Number, Mail, Phone:
+		return ValueType(typeOfValue), nil
+	default:
+		return "", errors.New("invalid typeOfValue, must be one of: number, mail, phone")
+	}
+}
+
 func SearchMain(value, typeOfValue, ip string) (string, error) {
 	// Check if the session is valid
 	session, _ := GetSessionByIP(ip)
@@ -27,18 +46,23 @@ func SearchMain(value, typeOfValue, ip string) (string, error) {
 	lastColumn := initializers.LastColumn
 
 	// Validate the value
+	validTypeOfValue, err := validateTypeOfValue(typeOfValue)
+	if err != nil {
+		return "", err
+	}
 	var valueColumn int
-	if typeOfValue == "email" {
+	switch validTypeOfValue {
+	case Mail:
 		valueColumn = initializers.MailRow
 		if !IsValidEmail(value) {
 			return "", errors.New("invalid email format")
 		}
-	} else if typeOfValue == "phone" {
+	case Phone:
 		valueColumn = initializers.PhoneRow
 		if !IsValidPhone(value) {
 			return "", errors.New("invalid phone format")
 		}
-	} else if typeOfValue == "number" {
+	case Number:
 		valueColumn = initializers.NumberRow
 		number, err := strconv.Atoi(value)
 		if err != nil {
@@ -47,7 +71,7 @@ func SearchMain(value, typeOfValue, ip string) (string, error) {
 		if number < value_min || number > value_max {
 			return "", errors.New("number out of range")
 		}
-	} else {
+	default:
 		return "", errors.New("invalid type")
 	}
 
