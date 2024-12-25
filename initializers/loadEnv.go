c94e356722e50791c7f2128c7d6f3775b23fee45
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-25 12:50:01 +0300

    Change error handling in LoadEnv to use fmt.Printf instead of log.Fatalf for environment variable validation

--- initializers/loadEnv.go
+++ initializers/loadEnv.go
@@ -1,6 +1,7 @@
 package initializers
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"strconv"
@@ -34,66 +35,66 @@ func LoadEnv() {
 	// Parse and assign environment variables to global variables
 	MailRow, err = strconv.Atoi(os.Getenv("MAIL_ROW"))
 	if err != nil {
-		log.Fatalf("Invalid MAIL_ROW environment variable: %v", err)
+		fmt.Printf("Invalid MAIL_ROW environment variable: %v", err)
 	}
 
 	PhoneRow, err = strconv.Atoi(os.Getenv("PHONE_ROW"))
 	if err != nil {
-		log.Fatalf("Invalid PHONE_ROW environment variable: %v", err)
+		fmt.Printf("Invalid PHONE_ROW environment variable: %v", err)
 	}
 
 	NumberRow, err = strconv.Atoi(os.Getenv("NUMBER_ROW"))
 	if err != nil {
-		log.Fatalf("Invalid NUMBER_ROW environment variable: %v", err)
+		fmt.Printf("Invalid NUMBER_ROW environment variable: %v", err)
 	}
 
 	ValueMin, err = strconv.Atoi(os.Getenv("VALUE_MIN"))
 	if err != nil {
-		log.Fatalf("Invalid VALUE_MIN environment variable: %v", err)
+		fmt.Printf("Invalid VALUE_MIN environment variable: %v", err)
 	}
 
 	ValueMax, err = strconv.Atoi(os.Getenv("VALUE_MAX"))
 	if err != nil {
-		log.Fatalf("Invalid VALUE_MAX environment variable: %v", err)
+		fmt.Printf("Invalid VALUE_MAX environment variable: %v", err)
 	}
 
 	SheetID = os.Getenv("SHEET_ID")
 	if SheetID == "" {
-		log.Fatal("SHEET_ID environment variable is not set")
+		fmt.Printf("SHEET_ID environment variable is not set")
 	}
 
 	PageName = os.Getenv("PAGE_NAME")
 	if PageName == "" {
-		log.Fatal("PAGE_NAME environment variable is not set")
+		fmt.Printf("PAGE_NAME environment variable is not set")
 	}
 
 	LastColumn = os.Getenv("LAST_COLUMN")
 	if LastColumn == "" {
-		log.Fatal("LAST_COLUMN environment variable is not set")
+		fmt.Printf("LAST_COLUMN environment variable is not set")
 	}
 
 	SMTP_User := os.Getenv("SMTP_USER")
 	if SMTP_User == "" {
-		log.Fatal("SMTP_USER environment variable is not set")
+		fmt.Printf("SMTP_USER environment variable is not set")
 	}
 
 	SMTP_Pass := os.Getenv("SMTP_PASS")
 	if SMTP_Pass == "" {
-		log.Fatal("SMTP_PASS environment variable is not set")
+		fmt.Printf("SMTP_PASS environment variable is not set")
 	}
 
 	SMTP_Host := os.Getenv("SMTP_HOST")
 	if SMTP_Host == "" {
-		log.Fatal("SMTP_HOST environment variable is not set")
+		fmt.Printf("SMTP_HOST environment variable is not set")
 	}
 
 	SMTP_Port := os.Getenv("SMTP_PORT")
 	if SMTP_Port == "" {
-		log.Fatal("SMTP_PORT environment variable is not set")
+		fmt.Printf("SMTP_PORT environment variable is not set")
 	}
 
 	SMS_MS := os.Getenv("SMS_MS")
 	if SMS_MS == "" {
-		log.Fatal("SMS_MS environment variable is not set")
+		fmt.Printf("SMS_MS environment variable is not set")
 	}
 }
