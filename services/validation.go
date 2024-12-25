dfb7e0469b2c377b04b33567310dcc21ccadedb3
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-12-25 13:26:02 +0300

    Add type validation for allowed values in validation service

--- services/validation.go
+++ services/validation.go
@@ -1,6 +1,28 @@
 package services
 
-import "regexp"
+import (
+	"errors"
+	"regexp"
+)
+
+// Define a custom type for the allowed values
+type ValueType string
+
+const (
+	Number ValueType = "number"
+	Mail   ValueType = "mail"
+	Phone  ValueType = "phone"
+)
+
+// Validate the typeOfValue
+func validateTypeOfValue(typeOfValue string) (ValueType, error) {
+	switch ValueType(typeOfValue) {
+	case Number, Mail, Phone:
+		return ValueType(typeOfValue), nil
+	default:
+		return "", errors.New("invalid typeOfValue, must be one of: number, mail, phone")
+	}
+}
 
 // isValidPhone validates the phone number format
 func IsValidPhone(phone string) bool {
