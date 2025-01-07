37824b155bdce38a0f1894baf4b6aa883735e205
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-01-07 17:25:10 +0300

    Initialize logging setup in main application

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/ibtikar-org-tr/iforgot/initializers"
+	"github.com/ibtikar-org-tr/iforgot/lib"
 	"github.com/ibtikar-org-tr/iforgot/routers"
 )
 
@@ -9,6 +10,7 @@ func init() {
 	initializers.LoadEnv()
 	initializers.ConnectDatabase()
 	initializers.SrvInit()
+	lib.SetupLogOutput()
 }
 
 func main() {
