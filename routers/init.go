abb7e17734d94767aeab22ebb3bf41fd66ed9586
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-01-07 17:25:13 +0300

    Add middleware for recovery and logging in router initialization

--- routers/init.go
+++ routers/init.go
@@ -2,11 +2,16 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/ibtikar-org-tr/iforgot/lib"
 )
 
 func AllRouters() {
 	router := gin.New()
 
+	// middleware
+	router.Use(gin.Recovery(), lib.Logger())
+
+	// register routes
 	RegisterStaticRoutes(router)
 	RegisterHTMLRoutes(router)
 
