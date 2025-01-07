e443fa2cdbaeade2d09b5e94991b7280a0961723
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-01-07 13:28:35 +0300

    Mask email address in search response and add confirmation message

--- handlers/search.go
+++ handlers/search.go
@@ -16,10 +16,22 @@ func SearchHandler(c *gin.Context) {
 	// log the request
 	fmt.Printf("Searching for value: %s, type: %s\n", value, typeOfValue)
 
-	result, err := services.SearchMain(value, typeOfValue, ip)
+	mail, err := services.SearchMain(value, typeOfValue, ip)
 	if err != nil {
 		c.JSON(500, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(200, gin.H{"result": result})
+
+	// hide part of the mail
+	result := mail[:4] + "****" + mail[len(mail)-14:]
+
+	// if the mail is shorter than 15 characters, show only 2 - 11
+	if len(mail) < 15 {
+		result = mail[:2] + "****" + mail[len(mail)-11:]
+	}
+
+	c.JSON(200, gin.H{
+		"mail":    result,
+		"message": "تمّ إرسال البيانات إلى البريد",
+	})
 }
